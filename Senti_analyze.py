8f95ae714d5e36ddf54c1c5e5243661ee79ed8de
Tianhe <ryan2214@users.noreply.github.com>
2020-11-05 06:07:54 +0800

    Update Senti_analyze.py

--- Senti_analyze.py
+++ Senti_analyze.py
@@ -2,9 +2,9 @@
 from TwitterAPI import TwitterAPI, TwitterPager
 
 # Imports the Google Cloud client library
-from google.cloud import language
-from google.cloud.language import enums
-from google.cloud.language import types
+#from google.cloud import language
+#from google.cloud.language import enums
+#from google.cloud.language import types
 
 # Instantiates a NLP client
 client = language.LanguageServiceClient()
