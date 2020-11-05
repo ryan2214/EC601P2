4b80de0ee7756dd85e8cb7d5759b44cfc213f6ae
Tianhe <ryan2214@users.noreply.github.com>
2020-11-05 09:55:27 +0800

    Update and rename Senti_analyze.py to senti_analyze.py

--- senti_analyze.py
+++ senti_analyze.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-from TwitterAPI import TwitterAPI, TwitterPager
+from TwitterAPI import TwitterAPI
 
 # Imports the Google Cloud client library
 from google.cloud import language
@@ -15,9 +15,9 @@ api = TwitterAPI(consumer_key,
                  consumer_secret,
                  auth_type='oAuth2')
 
-sentiResult = 0
-sentiNum = 0
-textTemp = u' '
+SENTI_RESULT = 0
+SENTI_NUM = 0
+TEXT_TEMP = u' '
 #Search x tweets about Trump
 
 r = api.request('search/tweets', {'q': SEARCH_TERM,'count':100, 'tweet_mode':"extended"})
@@ -25,21 +25,21 @@ r = api.request('search/tweets', {'q': SEARCH_TERM,'count':100, 'tweet_mode':"ex
 for item in r:
     if item['lang'] == 'en' :
 # The text to analyze
-        if 'extended_tweet' in item: 
+        if 'extended_tweet' in item:
             text = item['extended_tweet']['full_text']
-        if 'retweeted_status' in item: 
+        if 'retweeted_status' in item:
             text = item['retweeted_status']['full_text']
         elif 'full_text' in item:
             text = item['full_text']
         elif 'text' in item:
             text = item['text']
 
-        sentiNum += 1
-        textTemp = textTemp + text
+        SENTI_NUM += 1
+        TEXT_TEMP = TEXT_TEMP + text
 
 document = types.Document(content=textTemp, type=enums.Document.Type.PLAIN_TEXT)
 sentiment = client.analyze_sentiment(document=document).document_sentiment
 
-sentiResult = sentiment.score
+SENTI_RESULT = sentiment.score
 
-print("SentimentResult: {}".format(sentiResult))
+print("SentimentResult: {}".format(SENTI_RESULT))
