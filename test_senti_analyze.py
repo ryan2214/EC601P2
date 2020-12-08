71674d49d7b8327ebf476f042cccefd8639e7e3f
Tianhe <ryan2214@users.noreply.github.com>
2020-12-09 02:59:40 +0800

    Delete test_senti_analyze.py

--- test_senti_analyze.py
+++
@@ -1,9 +0,0 @@
-from senti_analyze import *
-
-# test about TwitterAPI
-twitter = TwitterAPI()
-
-def test_TwitterAPI_analyze_sentiment():
-    text = "Have a nice day!"
-    score = twitter.analyze_sentiment(text)
-    assert score > 0.25 # positive sentiment
